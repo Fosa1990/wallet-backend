dfd08e406ef7121a0551cdc64702cc51b72cd3e6
Bogdan-Kasyanchuk <bogdan.k1990@gmail.com>
2022-05-12 19:10:08 +0300

    delete-code-from-categories-routes

--- src/routes/api/categories.js
+++ src/routes/api/categories.js
@@ -1,23 +1,7 @@
 const router = require("express").Router();
 const { getAllCategories } = require("../../controllers");
 
-const { Category } = require("../../models");
-
 // http://localhost:8081/api/categories/getAll
 router.get("/", getAllCategories);
-
-// http://localhost:8081/api/transactions/createTransaction
-router.post("/", async (req, res, next) => {
-  const { body } = req;
-  const cat = await Category.create(body);
-  res.json({
-    status: "ок",
-    code: 200,
-    payload: {
-      message: "Transaction created successfully",
-      cat,
-    },
-  });
-});
 
 module.exports = router;
