8274875e3607c4ebfdd417d106eb9c17e6ec34ee
Fosa1990 <fosa1990@gmail.com>
2022-05-12 23:45:59 +0300

    completed verification user by email

--- src/controllers/users/verifyEmail.js
+++ src/controllers/users/verifyEmail.js
@@ -1,12 +1,24 @@
+const { NotFound } = require('http-errors');
+const { User } = require('../../models');
 const { STATUS, HTTP_CODE } = require('../../helpers/constants');
 
 // http://localhost:8081/api/users/verify/:verifyToken
-const verifyEmail = async (req, res, next) => {
-  res.json({
+const verifyEmail = async (req, res) => {
+  const { verifyToken } = req.params;
+
+  const userExist = await User.findOne({ verifyToken });
+
+  if (!userExist) throw new NotFound(`Not found user`);
+
+  userExist.verifyUser(true);
+  userExist.verifyToken(null);
+  await userExist.save();
+
+  return res.status(HTTP_CODE.OK).json({
     status: STATUS.SUCCESS,
     code: HTTP_CODE.OK,
     payload: {
-      message: 'template message: verify/:verifyToken',
+      message: `User: ${userExist.name} successfully verified`,
     },
   });
 };
