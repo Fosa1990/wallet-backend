56d701d0570131f199228ab9347908f8045adfff
Fosa1990 <fosa1990@gmail.com>
2022-05-16 07:46:38 +0300

    add template redirected when verify email to login page

--- src/controllers/users/verifyEmail.js
+++ src/controllers/users/verifyEmail.js
@@ -1,6 +1,7 @@
 const { NotFound } = require('http-errors');
 const { User } = require('../../models');
 const { STATUS, HTTP_CODE } = require('../../helpers/constants');
+// const { FRONTEND_URL } = process.env;
 
 // http://localhost:8081/api/users/verify/:verificationToken
 // METHOD: GET
@@ -14,6 +15,11 @@ const verifyEmail = async (req, res) => {
   userExist.verifyUser(true);
   userExist.verifyToken(null);
   await userExist.save();
+
+  // add redirect to login frontend page
+  // return res.redirect(
+  //   `${FRONTEND_URL}/api/auth/login?email=${userExist.email}&isVerified=${userExist.isVerified}`,
+  // );
 
   return res.status(HTTP_CODE.OK).json({
     status: STATUS.SUCCESS,
